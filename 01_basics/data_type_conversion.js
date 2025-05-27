3050f77891823c8589f2daa9a67abde7dbc19b46
Mohd AbuTalib <161178040+95070284@users.noreply.github.com>
2025-05-27 09:35:05 +0000

    conversion in datatype

---
+++ 01_basics/data_type_conversion.js
@@ -0,0 +1,60 @@
+let score="34"
+
+//console.log(score)
+//console.log(typeof(score))
+
+let scoreNumber=Number(score)
+//console.log(scoreNumber)
+//console.log(typeof scoreNumber)
+
+// String to number conversion ,where string is not a proper number.
+
+let nameWithLuckyNumber="5Raj"
+
+let nameInNumber=Number(nameWithLuckyNumber)
+//console.log(nameWithLuckyNumber)
+//console.log(typeof(nameWithLuckyNumber))
+//console.log(nameInNumber)
+//console.log(typeof(nameInNumber))
+
+
+/* any datatype to boolean  conversion */
+
+let marks=null
+
+let isRight=0
+let isRightInBoolean=Boolean(isRight)
+
+let marksInboolean=Boolean(marks)
+console.log(isRightInBoolean)
+
+
+
+
+// console.log(marksInboolean)
+// console.log(typeof(marksInboolean))
+
+// let name=undefined
+// console.log(name)
+// let nameInBoolean=Boolean(name)
+// console.log(nameInBoolean)
+// console.log(typeof(nameInBoolean))
+
+
+// let marksInString=String(marks)
+// console.log(marksInString)
+// console.log(typeof(marksInString))
+
+
+
+
+
+
+/* 
+"331a"-->NaN
+54-->"54"
+
+null--boolean-->false
+
+
+*/
