5c209293127d2f3b1722c3dce7bb33ddd1dae582
Mohd AbuTalib <161178040+95070284@users.noreply.github.com>
2025-05-27 07:32:09 +0000

    variables and datatype

---
+++ 01_basics/01_datatype.js
@@ -0,0 +1,21 @@
+const id=103
+let name="Mohd Talib"
+let age=22
+let status=null;
+
+console.table(typeof id)
+console.table(typeof name)
+console.table(typeof age)
+console.table(typeof status)
+console.log(typeof undefined)
+console.log(typeof Object)
+
+
+/*
+number
+string
+number
+object
+undefined
+function
+*/
---
+++ 01_basics/01_variable.js
@@ -0,0 +1,19 @@
+const employeeId=1222
+let emloyeeName="Mohd Talib"
+var email ="mohd4@gmail.com"
+empoyeeCity="New Delhi"
+
+console.log(emloyeeName)
+
+/*
+var : Please do not use of var .
+
+ignore> without keyword variable , this is implicit variable or unintended variable , generally we should avoid 
+this because it's made code unprecdictable.
+*/
+
+emloyeeName="Abu Talib"
+email="talismangmail.com"
+empoyeeCity="jaipur"
+
+console.table([employeeId, emloyeeName,email,empoyeeCity])
