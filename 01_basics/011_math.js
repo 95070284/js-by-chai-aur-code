2bc3904c9d94b2dd1fabd413b7259b0eef6c884a
Mohd AbuTalib <161178040+95070284@users.noreply.github.com>
2025-05-29 12:07:13 +0000

    numbers and math

---
+++ 01_basics/011_math.js
@@ -0,0 +1,31 @@
+let score=45
+
+// console.log(score)
+
+let newScore= new Number(87)
+// console.log(newScore)
+
+// console.log(newScore.toLocaleString())
+// console.log(newScore.toString())
+// console.log(newScore.toLocaleString().length)
+
+// ===========================================================================
+
+console.log(Math)
+
+let marks=69
+
+// console.log(`The use of absolute is convert any negative number into positive ,  ${Math.abs(-5)} it's not convert positive number into negative`)
+
+// console.log(Math.ceil(5.23))// it's increase the value with +1 , if after decimal is greater than 0
+// console.log(Math.ceil(7.6))
+// console.log(Math.ceil(4.0))
+
+// console.log(Math.PI)
+// console.log(Math.cbrt(3))// crbt use for cube root 
+// console.log(Math.floor(4.3))// floor ignore decimal value 
+// console.log(Math.floor(5.6))
+
+// console.log(Math.fround(5.6))//Returns the nearest single precision float representation of a number
+// console.log(Math.round(5.6))
+console.log(Math.floor((Math.random()*10000)+1))
