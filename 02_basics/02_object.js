7a9fcc65c4417a673edd226fea3d283e44e4f162
Mohd AbuTalib <161178040+95070284@users.noreply.github.com>
2025-05-30 23:34:58 +0000

    object sigleton

---
+++ 02_basics/02_object.js
@@ -0,0 +1,25 @@
+//singleton 
+
+// const fbUser=new Object()
+
+const fbUser= {}
+
+fbUser.name="mohdtalib"
+fbUser.id="@431"
+fbUser.password="raj324"
+
+
+// console.log(fbUser)
+
+
+let teleUser={
+    fullname:{
+        firstname:{
+            name:"Mohd Talib",
+            id:"mohd436"
+        }
+    }
+}
+
+// console.log(teleUser)
+console.log(teleUser.fullname.firstname)
