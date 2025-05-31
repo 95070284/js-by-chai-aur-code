7137477a1bcbc90c5df237853a9ebe9abbb0f8eb
Mohd AbuTalib <161178040+95070284@users.noreply.github.com>
2025-05-31 10:59:17 +0000

    iife interesting

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,11 @@
+// IIFE :IMMEDIATELY INVOKED FUNCTION EXPRESSION
+
+
+(function dbConnection (){
+    console.log("DB connected successfully" )
+
+})();
+
+((name)=>{
+    console.log(`db2 conneted successfully with ${name}`)
+})("mohd talib")
