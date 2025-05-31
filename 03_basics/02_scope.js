bcd402af977189845b1ffdf3a6257f6d56a20c50
Mohd AbuTalib <161178040+95070284@users.noreply.github.com>
2025-05-31 08:39:18 +0000

    scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -9,6 +9,52 @@ if(true){
     var c=40
 }
 
-console.log(a)
-console.log(b)
+// console.log(a)
+// console.log(b)
 // console.log(c)
+
+
+/*   https://scaling-waddle-g4xxpw57xvqp299gw.github.dev/    */
+
+function myAccount(){
+    const username="mohdtalib "
+
+    function myProfile(){
+        const follower=102
+        const like="❤️  1.K"
+
+        console.log(username+ like)
+    }
+
+    // console.log(follower)
+    myProfile()
+}
+
+// myAccount()
+
+
+
+if(true){
+    const username="mohdtalib"
+
+    if(true){
+        const channel=" youtube"
+        console.log(username+channel)
+    }
+    // console.log(channel)
+}
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num+1
+}
+
+// addTwo(9)   yaha se ye function hoisting work nahi karta hai .
+
+ const addTwo = function(num){
+    return num+2
+}
+
+console.log(addTwo(9))
