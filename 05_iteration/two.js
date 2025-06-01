ab1a30254569eb13b6e8da99776187468f2755ff
Mohd AbuTalib <161178040+95070284@users.noreply.github.com>
2025-06-01 05:32:35 +0000

    while ,dowhile loop basics

---
+++ 05_iteration/two.js
@@ -0,0 +1,15 @@
+let count=1
+while (count<=10) {
+    console.log(count)
+    count++
+}
+
+
+
+//==========================DO WHILE LOOP=============================================
+
+let num=11
+do {
+    console.log(num)
+    num++
+}while(num<=10)
